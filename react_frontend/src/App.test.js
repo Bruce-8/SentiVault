0dba1027f64ea031f7bb67597d0e646fc67b44ee
Bruce Liu <liuxb904@gmail.com>
2024-08-24 21:47:58 -0600

    Creating CI/CD workflow

--- react_frontend/src/App.test.js
+++ react_frontend/src/App.test.js
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders SentiVault title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/SentiVault/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// TODO: Incorporate front-end tests.
