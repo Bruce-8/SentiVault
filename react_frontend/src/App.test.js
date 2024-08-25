9a0c3bdee479b1786cd317f04ec91cc92ae5c6bd
Bruce Liu <liuxb904@gmail.com>
2024-08-24 18:08:53 -0600

    Created backend tests

--- react_frontend/src/App.test.js
+++ react_frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders SentiVault title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SentiVault/i);
   expect(linkElement).toBeInTheDocument();
 });
